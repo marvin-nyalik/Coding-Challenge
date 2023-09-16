b0809cfbd5defd10a52d25960f91065dc0bd2ab5
MARVIN <o.silverygold@gmail.com>
2023-09-16 18:34:53 +0300

    Add least common ancestor solution

--- subarray.js
+++ subarray.js
@@ -6,3 +6,14 @@ A subarray is a contiguous non-empty sequence of elements within an array
 
 */
 
+/*
+  count = 0
+  subarr = nums[0]
+  for(let i =0; i < nums.length; i++){
+    if sum(subarr) == k
+      count += 1
+    else{
+        subarr.push(subarr[i + 1])
+    }
+  }
+*/
