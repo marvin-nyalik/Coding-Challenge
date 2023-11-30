8566769768fc1cfa3ee3a169234feaf20dba2e29
MARVIN <o.silverygold@gmail.com>
2023-11-30 21:17:13 +0300

    Solve similar leaf nodes DFS

---
+++ similar_leaf.rb
@@ -0,0 +1,68 @@
+# Definition for a binary tree node.
+class TreeNode
+  attr_accessor :val, :left, :right
+
+  def initialize(val = 0, left = nil, right = nil)
+    @val = val
+    @left = left
+    @right = right
+  end
+end
+
+# using iteration
+def leaf_similar(root1, root2)
+  leaves1 = find_leaves(root1)
+  leaves2 = find_leaves(root2)
+
+  leaves1 == leaves2
+end
+
+def find_leaves(root)
+  return [] if root.nil?
+
+  leaves = []
+  stack = [root]
+
+  until stack.empty?
+    current = stack.pop
+
+    if current.left.nil? && current.right.nil?
+      leaves << current.val
+    end
+
+    stack.push(current.right) if current.right
+    stack.push(current.left) if current.left
+  end
+
+  leaves
+end
+
+Definition for a binary tree node.
+class TreeNode
+  attr_accessor :val, :left, :right
+
+  def initialize(val = 0, left = nil, right = nil)
+    @val = val
+    @left = left
+    @right = right
+  end
+end
+
+#using recursion
+def leaf_similar(root1, root2)
+    leaves1 = find_leaves(root1)
+    leaves2 = find_leaves(root2)
+  
+    leaves1 == leaves2
+  end
+  
+  def find_leaves(root)
+    return [] if root.nil?
+  
+    if root.left.nil? && root.right.nil?
+      [root.val]
+    else
+      find_leaves(root.left) + find_leaves(root.right)
+    end
+  end
+  
