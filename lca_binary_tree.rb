0b9197f100c33b90327f83d41b0c793b113e2328
MARVIN <o.silverygold@gmail.com>
2023-12-02 18:01:24 +0300

    Solution of BST lowest common ancestor

---
+++ lca_binary_tree.rb
@@ -0,0 +1,37 @@
+# # Definition for a binary tree node.
+# # class TreeNode
+# #     attr_accessor :val, :left, :right
+# #     def initialize(val)
+# #         @val = val
+# #         @left, @right = nil, nil
+# #     end
+# # end
+
+# # @param {TreeNode} root
+# # @param {TreeNode} p
+# # @param {TreeNode} q
+# # @return {TreeNode}
+# def lowest_common_ancestor(root, p, q)
+#     return root if p == root || q == root
+#     cur = root
+#     while cur
+#         if p.val < cur.val && q.val < cur.val
+#             cur = cur.left
+#         elsif p.val > cur.val && q.val > cur.val
+#             cur = cur.right
+#         else
+#             return cur
+#         end
+#     end
+# end
+
+def lowest_common_ancestor(root, p, q)
+    return root if root.nil? || p==root || q==root
+
+    left = lowest_common_ancestor(root.left, p, q)
+    right = lowest_common_ancestor(root.right, p, q)
+
+    return root if left && right 
+    
+    left || right
+end
