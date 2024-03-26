e8ca772276ebf7b5faa167f1db0d1da967c478c9
MARVIN <o.silverygold@gmail.com>
2024-03-26 20:19:56 +0300

    Update - Solve keys and rooms

--- keys_and_rooms.rb
+++ keys_and_rooms.rb
@@ -15,3 +15,23 @@ def can_visit_all_rooms(rooms)
     end
     rooms.size == visited.size
 end
+
+# @param {Integer[][]} rooms
+# @return {Boolean}
+# def can_visit_all_rooms(rooms)
+#     stack = [0]
+#     visited = Set.new
+#     visited.add(0)
+#     all_rooms = Set.new(0..rooms.size - 1)
+    
+#     until stack.empty?
+#         cur = stack.pop
+#         visited << cur if cur
+#         rooms[cur].each do |room|
+#             next if visited.include?(room)
+#             visited.add(room)
+#             stack << room
+#         end
+#     end
+#     all_rooms == visited
+# end
