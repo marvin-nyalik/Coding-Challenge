bce4b6907a92eab29cda97455f9167cb8c4affe9
MARVIN <o.silverygold@gmail.com>
2024-04-19 13:45:55 +0300

    Modern solution to good nodes problem

--- good_nodes.rb
+++ good_nodes.rb
@@ -15,3 +15,21 @@ def good_nodes(root)
     
     count
 end
+
+# Another trial
+def good_nodes(root)
+  return 0 if root.nil?
+  stack = [[root, root.val]]
+  count = 0
+
+  until stack.empty?
+      node, max_so_far = stack.pop
+      count += 1 if node.val >= max_so_far
+
+      max_so_far = [node.val, max_so_far].max
+      stack << [node.left, max_so_far] if node.left
+      stack << [node.right, max_so_far] if node.right
+  end
+
+  count
+end
