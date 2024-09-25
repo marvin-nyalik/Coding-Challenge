234803bf268b1e644ad9fe6fb72ecc2cf673da51
MARVIN NYALIK <o.silverygold@gmail.com>
2024-09-25 21:01:21 +0300

    Add JS solution

--- merge_alternately.rb
+++ merge_alternately.rb
@@ -24,3 +24,34 @@ def merge_alternately(word1, word2)
 end
 
 p merge_alternately('aaa', 'bbbbc')
+
+
+/**
+ * @param {string} word1
+ * @param {string} word2
+ * @return {string}
+ */
+var mergeAlternately = function(word1, word2) {
+    let i = 0;
+    let j = 0;
+    let s = [];
+
+    while (i < word1.length && j < word2.length){
+        s.push(word1[i])
+        s.push(word2[j])
+        i+=1
+        j+=1
+    }
+
+    while(i < word1.length){
+        s.push(word1[i])
+        i += 1;
+    }
+ 
+    while(j < word2.length){
+        s.push(word2[j])
+        j += 1;
+    }
+
+    return s.join('');
+};
